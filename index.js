e30a92baa5383ba2aaed3a2ca526a9dfe68fd307
Jono <jonocairns@gmail.com>
2016-10-14 22:22:06 +1300

    Initial commit

--- index.js
+++ index.js
@@ -13,8 +13,77 @@ commands.set(new RegExp(triggerPrefix + 'random', 'i'), ['function', playRandomS
 commands.set(new RegExp(triggerPrefix + 'popular', 'i'), ['function', sendPopularCommands]);
 commands.set(new RegExp(triggerPrefix + 'exit', 'i'), ['function', leaveVoiceChannel]);
 if(config.demoMode) {
-  commands.set(/liftoff/i, ['text', 'Houston, we have liftoff!']);
-  commands.set(/!smallstep/i, ['sound', 'smallstep.mp3']);
+  commands.set(/!liftoff/i, ['text', 'Hey look at me, I can suck my own Dee']);
+  commands.set(/!chairs/i, ['text', 'boys']);
+  commands.set(/!toby/i, ['text', 'Quality questions create a quality life. Successful people ask better questions, and as a result, they get better answers.']);
+  commands.set(/!wabbins/i, ['text', 'CREATE A VISION AND NEVER LET THE ENVIRONMENT, OTHER PEOPLE’S BELIEFS, OR THE LIMITS OF WHAT HAS BEEN DONE IN THE PAST SHAPE YOUR DECISIONS.']);
+  commands.set(/!tony/i, ['text', 'A real decision is measured by the fact that you’ve taken a new action. If there’s no action, you haven’t truly decided.']);
+
+  commands.set(/!crains/i, ['text', 'I will lift your spirits with my big black cock']);
+  commands.set(/!bekfast/i, ['sound', 'bekfast-short.mp3']);
+  commands.set(/!bfstlong/i, ['sound', 'bekfast-long.mp3']);
+  commands.set(/!sad/i, ['sound', '2sad4me.mp3']);
+  commands.set(/!horn/i, ['sound', 'airhorn.mp3']);
+  commands.set(/!say/i, ['sound', 'watchasay.mp3']);
+  commands.set(/!weed/i, ['sound', 'weed.mp3']);
+  commands.set(/!wow/i, ['sound', 'wow.mp3']);
+  commands.set(/!ross/i, ['sound', 'ross.mp3']);
+  commands.set(/!turndown/i, ['sound', 'turndown.mp3']);
+  commands.set(/!spooky/i, ['sound', 'spooky.mp3']);
+  commands.set(/!dedotaded/i, ['sound', 'dedotaded.mp3']);
+  commands.set(/!camera/i, ['sound', 'camera.mp3']);
+  commands.set(/!never/i, ['sound', 'never.mp3']);
+  commands.set(/!tripple/i, ['sound', 'tripple.mp3']);
+  commands.set(/!reese/i, ['sound', 'fart.wav']);
+  commands.set(/!wololo/i, ['sound', 'wololo.mp3']);
+  commands.set(/!vuvuzela/i, ['sound', 'vuvuzela.mp3']);
+  commands.set(/!prep/i, ['sound', 'prep.mp3']);
+  commands.set(/!haha/i, ['sound', 'haha.mp3']);
+  commands.set(/!muhaha/i, ['sound', 'muhaha.mp3']);
+  commands.set(/!hadouken/i, ['sound', 'hadouken.mp3']);
+  commands.set(/!fuckoff/i, ['sound', 'fuckoff.mp3']);
+  commands.set(/!ethan/i, ['sound', 'ethan.mp3']);
+  commands.set(/!dramatic/i, ['sound', 'dramatic.mp3']);
+  commands.set(/!crickets/i, ['sound', 'crickets.mp3']);
+  commands.set(/!cough/i, ['sound', 'cough.mp3']);
+  commands.set(/!bork/i, ['sound', 'bork.mp3']);
+  commands.set(/!whisper/i, ['sound', 'whisper.mp3']);
+  commands.set(/!hitler/i, ['sound', 'hitler.mp3']);
+  commands.set(/!inception/i, ['sound', 'inception.mp3']);
+
+  commands.set(/!nigga/i, ['sound', 'nigga.mp3']);
+  commands.set(/!surprise/i, ['sound', 'surprise.mp3']);
+  commands.set(/!gayyy/i, ['sound', 'gayyy.mp3']);
+  commands.set(/!nobody/i, ['sound', 'nobody.mp3']);
+  commands.set(/!cena/i, ['sound', 'cena.mp3']);
+  commands.set(/!murloc/i, ['sound', 'murloc.mp3']);
+  commands.set(/!turtles/i, ['sound', 'turtles.mp3']);
+  commands.set(/!shame/i, ['sound', 'shame.mp3']);
+  commands.set(/!love/i, ['sound', 'love.mp3']);
+  commands.set(/!allah/i, ['sound', 'allah.mp3']);
+  commands.set(/!clap/i, ['sound', 'clap.mp3']);
+  commands.set(/!duel/i, ['sound', 'duel.mp3']);
+  commands.set(/!suck/i, ['sound', 'suck.mp3']);
+  commands.set(/!how/i, ['sound', 'how.mp3']);
+  commands.set(/!dwagens/i, ['sound', 'dwagens.m4a']);
+  commands.set(/!dwagens1/i, ['sound', 'dwagens1.m4a']);
+  commands.set(/!dwagens2/i, ['sound', 'dwagens2.m4a']);
+
+  commands.set(/!rage/i, ['sound', 'rage.mp3']);
+  commands.set(/!mynigga/i, ['sound', 'mynigga.mp3']);
+  commands.set(/!spin/i, ['sound', 'spin.mp3']);
+  commands.set(/!smell/i, ['sound', 'smell.mp3']);
+  commands.set(/!choppa/i, ['sound', 'choppa.mp3']);
+  commands.set(/!yeahboy/i, ['sound', 'yeahboy.mp3']);
+  commands.set(/!money/i, ['sound', 'money.mp3']);
+  commands.set(/!epic/i, ['sound', 'epic.mp3']);
+  commands.set(/!justdoit/i, ['sound', 'justdoit.mp3']);
+  commands.set(/!tralala/i, ['sound', 'tralala.mp3']);
+  commands.set(/!yeeah/i, ['sound', 'yeeah.mp3']);
+  commands.set(/!boondocks/i, ['sound', 'boondocks.mp3']);
+  commands.set(/!shesaid/i, ['sound', 'shesaid.mp3']);
+  commands.set(/!men/i, ['sound', 'men.mp3']);
+  commands.set(/!gayaf/i, ['sound', 'reese.mp3']);
 }
 // commands.set(//i, ['', '']);
 
@@ -67,7 +136,7 @@ function addSoundsTo(map, fromDirectoryPath) {
 }
 
 function sendMessage(authorChannel, text) {
-  bot.sendMessage(authorChannel, text);
+  bot.sendTTSMessage(authorChannel, text);
 }
 
 function leaveVoiceChannel(message) {
@@ -136,6 +205,17 @@ function playRandomSound(message) {
   playSound(message.channel, message.author.voiceChannel, regExpToCommand(randomKey), randomValue[1]);
 }
 
+function isUserBanned(username) {
+    var banlist = config.banList.split(',');
+    var isBanned = false;
+    banlist.forEach(function(bannedUsername){
+        if(bannedUsername === username) {
+          isBanned = true;
+        }
+    });
+    return isBanned;
+}
+
 function displayCommands(message) {
   var helpMessage = '';
 
@@ -155,7 +235,8 @@ function displayCommands(message) {
 }
 
 bot.on('message', function(message) {
-  if(message.author.username !== bot.user.username) {
+
+  if(message.author.username !== bot.user.username && !isUserBanned(message.author.username)) {
     commands.forEach(function (botReply, regexp) {
       if(message.content.match(regexp)) {
         switch(botReply[0]) {
@@ -176,12 +257,52 @@ bot.on('message', function(message) {
   }
 });
 
+
 (function init() {
   bot.loginWithToken(config.botToken);
 
   if(config.autoLoadSounds) {
     addSoundsTo(commands, config.soundPath);
   }
+
+  bot.on('voiceSwitch', function(oldChannel, newChannel, user) {
+      var messageChannel = bot.channels.get("name", "hashfag");
+
+      switch(user.username) {
+        case 'jonosma':
+            playSound(messageChannel, newChannel, regExpToCommand("!bekfast"), "bekfast-short.mp3");
+            break;
+        case 'ddynz':
+            playSound(messageChannel, newChannel, regExpToCommand("!men"), "men.mp3");
+            break;
+        case 'J1s':
+            playSound(messageChannel, newChannel, regExpToCommand("!reese"), "reese.mp3");
+            break;
+        case 'Commix':
+            playSound(messageChannel, newChannel, regExpToCommand("!surprise"), "surprise.mp3");
+            break;
+        case 'The Penetrator':
+            playSound(messageChannel, newChannel, regExpToCommand("!inception"), "inception.mp3");
+            break;
+        case 'joshr4h':
+            playSound(messageChannel, newChannel, regExpToCommand("!wololo"), "wololo.mp3");
+            break;
+        case 'jamie':
+            playSound(messageChannel, newChannel, regExpToCommand("!allah"), "allah.mp3");
+            break;
+        case 'Vashkar':
+            playSound(messageChannel, newChannel, regExpToCommand("!smell"), "smell.mp3");
+            break;
+        case 'Vashkar':
+            playSound(messageChannel, newChannel, regExpToCommand("!smell"), "smell.mp3");
+            break;
+        case 'Onez':
+            playSound(messageChannel, newChannel, regExpToCommand("!cena"), "cena.mp3");
+            break;
+        default:
+            playSound(messageChannel, newChannel, regExpToCommand("!ethan"), "ethan.mp3");
+      }
+  });
 
   loadStatsFile();
 })();
