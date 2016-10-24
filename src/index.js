a5af6a4f5026e309597674b6ccbef0f6c1beaf1e
Jono <jonocairns@gmail.com>
2016-10-24 16:24:11 +1300

    Fixing promise on message deletion

--- src/index.js
+++ src/index.js
@@ -124,6 +124,11 @@ class Dank {
 	static clear(message) {
 		const splitty = message.content.split(' ');
 		let numberToPurge = 10;
+
+		if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+			message.channel.sendMessage('Fuck off and check your privilege.');
+			return;
+		}
 		if (splitty.length > 1) {
 			numberToPurge = message.content.split(' ')[1];
 			const isNumber = !isNaN(parseFloat(numberToPurge)) && isFinite(numberToPurge);
@@ -134,20 +139,21 @@ class Dank {
 			numberToPurge = parseInt(numberToPurge, 10);
 		}
 
-		const ms = message.channel.sendMessage('Cleanup on aisle five');
-		ms.delete(2000).then(() => {
-			message.channel.fetchMessages({ limit: numberToPurge }).then((messagesToDelete) => {
-				messagesToDelete.deleteAll().then(() => {
-					const fin = message.channel.sendMessage('RIP in peace');
-					fin.delete(2000);
-				}).catch((e) => {
-					message.channel.sendMessage(':face_palm: I might not have the right permissions to do that m8ty.');
-					console.log(e);
+		message.channel.sendMessage('Cleanup on aisle five').then((ms) => {
+			ms.delete(2000).then(() => {
+				message.channel.fetchMessages({ limit: numberToPurge }).then((messagesToDelete) => {
+					messagesToDelete.deleteAll().then(() => {
+						message.channel.sendMessage('RIP in peace').then((fin) => {
+							fin.delete(2000);
+						});
+					}).catch((e) => {
+						message.channel.sendMessage(':face_palm: I might not have the right permissions to do that m8ty.');
+						console.log(e);
+					});
 				});
 			});
 		});
 	}
-
 
 	speech(message) {
 		const obj = Tts.process(message, this.commands);
