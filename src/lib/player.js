f440fb8bbcf2059a5a9bd66b2aa6c8aae75de76b
Jono <jonocairns@gmail.com>
2016-11-06 17:56:54 +1300

    Adding error handling for yt stream

--- src/lib/player.js
+++ src/lib/player.js
@@ -94,6 +94,20 @@ class Player {
 		.then((connection) => {
 			console.log('Connected to voice channel... Attempting to play video');
 			const stream = ytdl(url);
+			stream.on('error', (error) => {
+				message.channel.sendMessage('Some bad shit went down. Probs some shit with yo shitty youtube video. RIP');
+				console.log(error);
+				error.destroy();
+			});
+
+			stream.on('response', (res) => {
+				if (res.statusCode === 403) {
+					message.channel.sendMessage('Can\'t play that shitty video..');
+					console.log(`code:${res.statusCode} message:${res.statusMessage}`);
+					console.log(res);
+					res.destroy();
+				}
+			});
 
 			const dispatcher = connection.playStream(stream, streamOptions);
 			dispatcher.on('error', err => console.log('Error occured attempting to stream', err));
