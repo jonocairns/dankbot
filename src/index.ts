2af7e03a362825e3ded0eff84e683b796c48c1dd
Jono Cairns <jonocairns@gmail.com>
2019-10-14 20:04:28 +1300

    Migrate to typescript

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import Discord from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const client = new Discord.Client();
+
+
+console.log(process.env);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
