9901bafba105968b94b2fe3dc35b51e5419e8693
Jono Cairns <jonocairns@gmail.com>
2019-10-15 19:05:54 +1300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,62 @@
 import Discord from 'discord.js';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 const client = new Discord.Client();
+const sounds: Array<string> = [];
 
-
-console.log(process.env);
+fs.readdir(path.join(__dirname, '../sounds'), (err, files) => {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+  files.forEach(file => sounds.push(file));
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (!msg.content.startsWith('.')) return;
+  if (!msg.guild) return;
+
+  if (msg.member.voiceChannel) {
+    msg.member.voiceChannel
+      .join()
+      .then(connection => {
+        let targetFile = msg.content.split('.').pop();
+
+        if (targetFile === 'leave') {
+          msg.member.voiceChannel.leave();
+        }
+
+        if (targetFile === 'meme') {
+          targetFile = sounds[Math.floor(Math.random() * sounds.length)]
+            .split('.')
+            .shift();
+        }
+
+        const file = sounds.find(s => s.split('.').shift() === targetFile);
+
+        if (file) {
+          const dispatcher = connection.playFile(
+            path.join(__dirname, `../sounds/${file}`)
+          );
+
+          dispatcher.on('end', () => {
+            msg.delete();
+          });
+
+          dispatcher.on('error', e => {
+            console.log(e);
+          });
+        }
+      })
+      .catch(console.log);
+  } else {
+    msg.reply('You need to join a voice channel first!');
   }
 });
 
