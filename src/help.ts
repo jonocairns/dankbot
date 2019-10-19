3ccc95543ca7d6e6e37cc0a33d0c03db8d77f526
Jono Cairns <jonocairns@gmail.com>
2019-10-20 12:25:13 +1300

    add voice commands

--- src/help.ts
+++ src/help.ts
@@ -1,5 +1,7 @@
 import Discord from 'discord.js';
 
+import {languages} from './speech';
+
 export const help = (msg: Discord.Message) => {
   const embed = new Discord.RichEmbed()
     .addField(
@@ -15,6 +17,17 @@ export const help = (msg: Discord.Message) => {
       'this is to be used in combination with a youtube URL e.g. `.yt https://youtube.com/watch?v=yourvideo`'
     )
     .addField('.eg', 'will return a sample of the possible sounds you can play')
+    .addField(
+      '.speak',
+      'will say a some words in the voice channel you are in e.g. `.speak i love lamp`'
+    )
+    .addField(
+      '.lang',
+      `will switch the voice acent, possible values are: ${languages.join(
+        ', '
+      )} e.g. \`.lang english\``
+    )
+    .addField('.joke', 'will say a random joke in the current voice channel')
     .addField(
       '.leave',
       `will make the bot leave the voice channel they're currently in`
