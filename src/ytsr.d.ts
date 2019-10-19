f11a004705707e9ed3bd16dcd8ef5d65cbad3586
Jono Cairns <jonocairns@gmail.com>
2019-10-19 23:06:47 +1300

    Add youtube search and languages

---
+++ src/ytsr.d.ts
@@ -0,0 +1,25 @@
+interface Result {
+  link: string;
+  title: string;
+}
+
+interface SearchResult {
+  query: string;
+  items: Array<Result>;
+}
+
+declare function ytsr(
+  searchString: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  options: any,
+  callback: (error: Error, result: SearchResult) => void
+): {};
+
+declare namespace ytsr {
+  function getFilters(
+    searchString: string,
+    callback: (error: Error, result: SearchResult) => void
+  ): {};
+}
+
+export default ytsr;
