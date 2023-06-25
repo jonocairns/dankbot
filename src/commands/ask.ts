c58d53cce1b1c1de0fcb0828422d72363bf64ff3
Jono Cairns <jonocairns@gmail.com>
2023-06-25 18:59:56 +1200

    Add TTS for voice channel + chat GPT

---
+++ src/commands/ask.ts
@@ -0,0 +1,75 @@
+import {CacheType, ChatInputCommandInteraction, SlashCommandBuilder} from 'discord.js';
+import {Command, CommandName} from '../util';
+import {createAudioResource} from '@discordjs/voice';
+import {Configuration, OpenAIApi} from 'openai';
+import {getPlayer} from '../getPlayer';
+import axios from 'axios';
+
+const AI_MODEL = 'text-davinci-003';
+
+const CLONED_VOICES_API_URL = 'https://play.ht/api/v2/cloned-voices';
+const TTS_STREAM_API_URL = 'https://play.ht/api/v2/tts/stream';
+
+const VOICE = 's3://voice-cloning-zero-shot/f155547d-abae-4349-b1e8-7efa48bf3bc8/trump-short/manifest.json';
+
+export const ask: Command = {
+	id: CommandName.ask,
+	register: new SlashCommandBuilder()
+		.setName(CommandName.ask)
+		.setDescription('Ask me anything')
+		.addStringOption((option) => option.setName('ask').setDescription('The thing to ask')),
+	async run(interaction: ChatInputCommandInteraction<CacheType>) {
+		const prompt = interaction.options.get('ask')?.value as string;
+
+		const configuration = new Configuration({
+			apiKey: process.env.OPENAI_API_KEY,
+		});
+		const openai = new OpenAIApi(configuration);
+		const completion = await openai.createCompletion({
+			model: AI_MODEL,
+			prompt: `respond to the following question like donald trump would but include shit talking and offensive language: "${prompt}"`,
+			max_tokens: 2048,
+		});
+
+		const text = completion.data.choices[0].text;
+
+		console.log(`response: ${text}`);
+
+		const payload = {
+			text,
+			voice: VOICE,
+			speed: 1,
+			quality: 'draft',
+		};
+
+		const response = await axios.post(TTS_STREAM_API_URL, payload, {
+			headers: {
+				'AUTHORIZATION': `Bearer ${process.env.PLAY_HT_API_KEY}`,
+				'X-USER-ID': process.env.PLAY_HT_USER_ID ?? '',
+				'ACCEPT': 'audio/mpeg',
+				'CONTENT-TYPE': 'application/json',
+			},
+			responseType: 'stream',
+		});
+
+		const {player} = getPlayer(interaction);
+		const resource = createAudioResource(response.data);
+		player.play(resource);
+
+		await interaction.editReply(text ?? 'You are welcome.');
+	},
+};
+
+export const getClonedVoices = async () => {
+	const options = {
+		headers: {
+			'AUTHORIZATION': `Bearer ${process.env.PLAY_HT_API_KEY}`,
+			'X-USER-ID': process.env.PLAY_HT_USER_ID ?? '',
+			'ACCEPT': 'application/json',
+		},
+	};
+
+	const voices = await axios.get(CLONED_VOICES_API_URL, options);
+
+	console.log(voices);
+};
--- src/getPlayer.ts
+++ src/getPlayer.ts
@@ -2,38 +2,35 @@ import {createAudioPlayer, joinVoiceChannel} from '@discordjs/voice';
 import {CacheType, ChatInputCommandInteraction, GuildMember} from 'discord.js';
 import {logger} from './logger';
 
-const UNABLE_TO_CONNECT_ERROR =
-    'I cannot play sounds due to reasons above what your simple mind can handle';
+const UNABLE_TO_CONNECT_ERROR = 'I cannot play sounds due to reasons above what your simple mind can handle';
 
-export const getPlayer = (
-    interaction: ChatInputCommandInteraction<CacheType>
-) => {
-    const {member, guild, guildId} = interaction;
-    const voiceChannel = (member as GuildMember)?.voice?.channel?.id;
-    const player = createAudioPlayer();
+export const getPlayer = (interaction: ChatInputCommandInteraction<CacheType>) => {
+	const {member, guild, guildId} = interaction;
+	const voiceChannel = (member as GuildMember)?.voice?.channel?.id;
+	const player = createAudioPlayer();
 
-    const canJoin = !voiceChannel || !guildId || !guild?.voiceAdapterCreator;
+	const canJoin = !voiceChannel || !guildId || !guild?.voiceAdapterCreator;
 
-    if (canJoin) {
-        interaction.reply({
-            content: UNABLE_TO_CONNECT_ERROR,
-        });
-        throw new Error(`Unable to join voice channel`);
-    }
+	if (canJoin) {
+		interaction.reply({
+			content: UNABLE_TO_CONNECT_ERROR,
+		});
+		throw new Error(`Unable to join voice channel`);
+	}
 
-    const connection = joinVoiceChannel({
-        channelId: voiceChannel,
-        guildId: guildId,
-        adapterCreator: guild.voiceAdapterCreator,
-        selfDeaf: false,
-        selfMute: false,
-    });
+	const connection = joinVoiceChannel({
+		channelId: voiceChannel,
+		guildId: guildId,
+		adapterCreator: guild.voiceAdapterCreator,
+		selfDeaf: false,
+		selfMute: false,
+	});
 
-    connection.subscribe(player);
+	connection.subscribe(player);
 
-    player.on('error', (error) => {
-        logger.error(error);
-    });
+	player.on('error', (error) => {
+		logger.error(error);
+	});
 
-    return {player, connection};
+	return {player, connection};
 };
