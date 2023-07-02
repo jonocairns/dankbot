d8b7f75436f6b96bde0401093241b66387ca405e
Jono Cairns <jonocairns@gmail.com>
2023-07-02 18:41:11 +1200

    Alter voices slightly

--- src/prompts/default.ts
+++
@@ -1,6 +0,0 @@
-import {ChatCompletionRequestMessage} from 'openai';
-
-export const defaultPrompt = (name: string): ChatCompletionRequestMessage => ({
-	role: 'system',
-	content: `You are ${name} and answer all questions as if you were them. Emphasising known intricacies of their personality. Your replies include offensive language and are verbally hostile to the person asking the questions while also being hilarious.`,
-});
